d8431440c6f106ca6d03ccbe850b20029b6d2fb4
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-08-15 11:58:25 +0100

    implemented loading feature

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import axiosInstance from "../api/axios";
+import Loading from "./Loading";
 
 const Blogs = () => {
   const [posts, setPosts] = useState([]);
   const [visible, setVisible] = useState(3);
+  const [isLoaded, setIsLoaded] = useState(false)
 
   const loadMore = () => {
     setVisible((item) => item + 3);
@@ -14,9 +16,11 @@ const Blogs = () => {
       try {
         const results = await axiosInstance.get(`?&offset=${visible}`);
         setPosts(results.data.results);
+        setIsLoaded(true)
         console.log(results);
       } catch {
         console.log("Error getting blogs");
+        setIsLoaded(true)
       }
     };
     getBlog();
@@ -25,8 +29,8 @@ const Blogs = () => {
   return (
     <>
       <div className="blogs">
-        <h1>Blogs...</h1>
-        {posts.map((post) => {
+        <h1>Blogs</h1>
+        {isLoaded ? posts.map((post) => {
           return (
             <div className="blog-container" key={post?.id}>
               <div className="blog-content-conatiner">
@@ -40,7 +44,7 @@ const Blogs = () => {
               </div>
             </div>
           );
-        })}
+        }): <Loading />}
         <div className="blog-loadmore">
           <button className="loadmore" onClick={() => loadMore()}>
             load more
